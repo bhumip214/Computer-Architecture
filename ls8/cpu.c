fb5c66a4f9ff8ccc487b45e4cf928bec908691d7
Bhumi Patel <bhumip214@gmail.com>
2019-06-11 13:05:03 -0700

    Un-hardcode the machine code

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -20,26 +20,56 @@ unsigned char cpu_ram_write(struct cpu *cpu, unsigned char address, unsigned cha
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
-void cpu_load(struct cpu *cpu)
+void cpu_load(struct cpu *cpu, char *filename)
 {
-  char data[DATA_LEN] = {
-      // From print8.ls8
-      0b10000010, // LDI R0,8
-      0b00000000,
-      0b00001000,
-      0b01000111, // PRN R0
-      0b00000000,
-      0b00000001 // HLT
-  };
+  // char data[DATA_LEN] = {
+  //     // From print8.ls8
+  //     0b10000010, // LDI R0,8
+  //     0b00000000,
+  //     0b00001000,
+  //     0b01000111, // PRN R0
+  //     0b00000000,
+  //     0b00000001 // HLT
+  // };
 
+  // int address = 0;
+
+  // for (int i = 0; i < DATA_LEN; i++)
+  // {
+  //   cpu->ram[address++] = data[i];
+  // }
+
+  // TODO: Replace this with something less hard-coded
+  FILE *fp;
+  char line[1024];
   int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++)
+  fp = fopen(filename, "r");
+
+  if (fp == NULL)
   {
-    cpu->ram[address++] = data[i];
+    fprintf(stderr, "file not found\n");
+    exit(1);
   }
 
   // TODO: Replace this with something less hard-coded
+  while (fgets(line, 1024, fp) != NULL)
+  { // not EOF
+    char *endptr;
+
+    unsigned char v = strtoul(line, &endptr, 10);
+
+    if (endptr == line)
+    {
+      continue;
+    }
+    //printf("%s", line);
+    //printf("%u\n", v);
+    cpu->ram[address] = v;
+    address++;
+  }
+
+  fclose(fp);
 }
 
 /**
